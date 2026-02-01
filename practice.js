d9b8604498c1d6cdd2f301f0aede2b0b22384690
lokesh <inglelokesh20@gmail.com>
2026-02-01 23:59:39 +0530

    changed in practice.js

--- practice.js
+++ practice.js
@@ -10,3 +10,12 @@ console.log(newStr);
 ///----------
 
 console.log("Another testing for console...")
+let arr = [1,2,3,4,5,6];
+for(let i = 0, j=arr.length-1; i<j; i++,j--){
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+console.warn(arr);
+console.log(arr);
